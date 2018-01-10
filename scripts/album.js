8154a38fbdf51152890bd95a1119bbf3863848f7
Katie Colasacco <caitlincolasacco@gmail.com>
2018-01-09 21:30:56 -0500

    Assignment 25 DOM Scripting completed

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@
      ]
  };
 
+ // Third Album for Assignment #25
+ var albumMine = {
+     title: 'This One is Mine',
+     artist: 'John Smith',
+     label: 'The Best Label',
+     year: '2007',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Firstly', duration: '2:00' },
+         { title: 'Secondly', duration: '3:57' },
+         { title: 'Thirdly', duration: '4:14'},
+         { title: 'Fourthly', duration: '2:07' },
+         { title: 'Fifthly', duration: '2:12'}
+     ]
+ };
+
  var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -42,24 +58,24 @@
      return template;
  };
 
- var setCurrentAlbum = function(album) {
-     // #1
-     var albumTitle = document.getElementsByClassName('album-view-title')[0];
-     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-     var albumImage = document.getElementsByClassName('album-cover-art')[0];
-     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+// Album elements to be populated with text dynamically
+ var albumTitle = document.getElementsByClassName('album-view-title')[0];
+ var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+ var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+ var albumImage = document.getElementsByClassName('album-cover-art')[0];
+ var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
-     // #2
+ var setCurrentAlbum = function(album) {
+     // assign values to each part of the album (text, images)
      albumTitle.firstChild.nodeValue = album.title;
      albumArtist.firstChild.nodeValue = album.artist;
      albumReleaseInfo.firstChild.nodeValue = album.year + ' ' + album.label;
      albumImage.setAttribute('src', album.albumArtUrl);
 
-     // #3
+     // clear contents of album song list container
      albumSongList.innerHTML = '';
 
-     // #4
+     // build list of songs from album JavaScript object
      for (var i = 0; i < album.songs.length; i++) {
          albumSongList.innerHTML += createSongRow(i + 1, album.songs[i].title, album.songs[i].duration);
      }
@@ -67,4 +83,14 @@
 
  window.onload = function() {
      setCurrentAlbum(albumPicasso);
- };
+
+     var albums = [albumPicasso, albumMarconi, albumMine];
+     var index = 1;
+     albumImage.addEventListener("click", function(event) {
+      setCurrentAlbum(albums[index]);
+      index++;
+      if (index === albums.length) {
+        index = 0;
+      }
+    });
+  };
