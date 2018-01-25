619908b3a99f5d1fe855b64920aa4304c18453ba
Katie Colasacco <caitlincolasacco@gmail.com>
2018-01-24 20:58:16 -0500

    Checkpoint 32 jQuery Next/Previous Completed

---
+++ scripts/fixtures.js
@@ -0,0 +1,47 @@
+// Example Album
+ var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ // Another Example Album
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ // Third Album for Assignment #25
+ var albumMine = {
+     title: 'This One is Mine',
+     artist: 'John Smith',
+     label: 'The Best Label',
+     year: '2007',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Firstly', duration: '2:00' },
+         { title: 'Secondly', duration: '3:57' },
+         { title: 'Thirdly', duration: '4:14'},
+         { title: 'Fourthly', duration: '2:07' },
+         { title: 'Fifthly', duration: '2:12'}
+     ]
+ };
